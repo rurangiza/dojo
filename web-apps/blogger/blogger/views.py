3df434535c43a5c21c934be5dc707efb12730dbe
Arsene <a.rurangiza@gmail.com>
2024-02-19 10:57:21 +0100

    cleanup

--- web-apps/blogger/blogger/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(req):
-    return render(req, 'homepage.html')
-
-def about(req):
-    return render(req, 'about.html')
